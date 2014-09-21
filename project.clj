9bbe9ec4c48152be3fbb8ccfa5c60b024967d362
Robert Stuttaford <robert@cognician.com>
2014-09-21 12:18:37 +0200

    Logback logging.

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-core "1.3.0"]
                  [ring/ring-jetty-adapter "1.3.0"]
+                 [org.clojure/tools.logging "0.3.0"]
+                 [ch.qos.logback/logback-classic "1.1.2"]
                  [compojure "1.1.8"]
                  [hiccup "1.0.5"]
                  [enlive "1.1.5"]
@@ -16,7 +18,9 @@
                  [prismatic/plumbing "0.3.3"]
                  [me.raynes/conch "0.8.0"]
                  [me.raynes/fs "1.4.4"]
-                 [com.datomic/datomic-free "0.9.4880.6"]]
+                 [com.datomic/datomic-free "0.9.4880.6"
+                  :exclusions [org.slf4j/slf4j-nop]]]
   :aliases {"generate" ["run" "-m" "stuttaford.generate"]}
+  :jvm-opts ["-Dlogback.configurationFile=logback.xml"]
   :profiles {:dev {:source-paths ["dev"]
                    :main         user}})
