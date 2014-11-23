79c68b207a9d182f2f80b9f1b8ad3d1de44a36e2
Robert Stuttaford <robert@cognician.com>
2014-11-23 12:26:04 +0200

    Publish articles.

--- resources/public/js/src/stuttaford/radiant/transactions.cljs
+++
@@ -1,9 +0,0 @@
-(ns stuttaford.radiant.transactions
-  (:require [datascript :as d]
-            [om-tools.core :refer-macros [defcomponentk]]
-            [sablono.core :as html :refer-macros [html]]
-            [stuttaford.radiant.components :as components]))
-
-(defcomponentk transactions [data owner]
-  (render-state [_ state]
-    (html [:div "Transactions"])))
