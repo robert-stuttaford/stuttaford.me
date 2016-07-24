a625fb5a24ced582022202e42f4bc3cb47444048
Robert Stuttaford <robert@cognician.com>
2016-07-24 12:33:54 +0200

    Redo Codex. Clear away Radiant and other old stuff. Update deps.

--- js-debug/dive-into-datomic.js
+++
@@ -1,2 +0,0 @@
-goog.require('stuttaford.dive_into_datomic.content');
-goog.require('stuttaford.dive_into_datomic');
--- js-debug/src/om_tools/mixin.js
+++
@@ -1,5 +0,0 @@
-goog.provide('om_tools.mixin');
-goog.require('cljs.core');
-goog.require('om.core');
-goog.require('om.core');
-//# sourceMappingURL=mixin.js.map
